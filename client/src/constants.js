d06bff23603b281820156a174e1b6332f95dba8d
Akshat Garg <akshatarungarg78@gmail.com>
2021-01-20 16:40:52 +0530

    switch back to deploy url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
 export const URL = {
   dev: "http://localhost:5000",
   prod: "https://human-resource-backend.herokuapp.com",
-  use: "http://localhost:5000",
+  use: "https://human-resource-backend.herokuapp.com",
 };
 
 export const USER_REQUESTS_TYPE = {
